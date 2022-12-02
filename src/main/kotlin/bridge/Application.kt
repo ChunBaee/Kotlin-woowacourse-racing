53f342de2270a38f742fcdaba25a7918a287dff6
ChunBaee <mljung10@naver.com>
2022-12-02 18:26:36 +0900

    feat: 게임을 시작하면서 자동차 이름 입력 안내 텍스트 출력하기

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,6 +1,10 @@
 package bridge
 
 fun main() {
+    startRacingGame()
+}
 
+private fun startRacingGame() {
+    PrintForm().noticeInputCarName()
 }
 
---
+++ src/main/kotlin/bridge/PrintForm.kt
@@ -0,0 +1,9 @@
+package bridge
+
+class PrintForm {
+
+    fun noticeInputCarName() {
+        println("경주할 자동차 이름을 입력하세요.(이름은 쉽표(,) 기준으로 구분)")
+    }
+
+}
