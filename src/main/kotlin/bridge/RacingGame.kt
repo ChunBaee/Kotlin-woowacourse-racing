64d0f1f697c2f538bf7e86801afdc05572c824c0
ChunBaee <mljung10@naver.com>
2022-12-02 18:59:58 +0900

    feat: 최종 우승자를 가리기 위해 Car 배열 내에서 가장 큰 Position의 값 가져오기

--- src/main/kotlin/bridge/RacingGame.kt
+++ src/main/kotlin/bridge/RacingGame.kt
@@ -1,21 +1,30 @@
 package bridge
 
-class RacingGame (private val carList : List<Car>, private val tryCount : Int) {
+class RacingGame(private val carList: List<Car>, private val tryCount: Int) {
 
     fun loopRacingGame() {
         PrintForm().noticePlayResultMention()
-        for(i in 0 until tryCount) {
+        for (i in 0 until tryCount) {
             //각 게임의 매 회차
             loopForEachCars()
             PrintForm().noticePlayResult(carList)
         }
+        printWinner()
     }
 
     private fun loopForEachCars() {
-        for(i in carList.indices) {
-            if(ReturnRandom().returnRandom() in 4..9) {
+        for (i in carList.indices) {
+            if (ReturnRandom().returnRandom() in 4..9) {
                 carList[i].position++
             }
         }
+    }
+
+    private fun printWinner() {
+        checkWhoIsWinner()
+    }
+
+    private fun checkWhoIsWinner() {
+        val maxLength = carList.maxWithOrNull(Comparator.comparingInt { it.position })?.position
     }
 }
