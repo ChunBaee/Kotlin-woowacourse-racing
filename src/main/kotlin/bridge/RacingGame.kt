7846af508ca61c30e56afc42990061c6c2b1c735
ChunBaee <mljung10@naver.com>
2022-12-02 18:54:24 +0900

    feat: 각 게임 회차마다 동작할 자동차 별 동작 반복문 작성

--- src/main/kotlin/bridge/RacingGame.kt
+++ src/main/kotlin/bridge/RacingGame.kt
@@ -6,6 +6,13 @@ class RacingGame (private val carList : List<Car>, private val tryCount : Int) {
         PrintForm().noticePlayResultMention()
         for(i in 0 until tryCount) {
             //각 게임의 매 회차
+            loopForEachCars()
+        }
+    }
+
+    private fun loopForEachCars() {
+        for(i in carList.indices) {
+            //매 회차 자동차별로 동작
         }
     }
 }
