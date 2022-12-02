788463f81420d79731acab66784c01025a7667e4
ChunBaee <mljung10@naver.com>
2022-12-02 18:50:23 +0900

    feat: 자동차명이 담긴 배열을 Car 객체 배열로 변환해주는 로직 추가

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -5,7 +5,15 @@ fun main() {
 }
 
 private fun startRacingGame() {
-    val userCarLit = UserInput().racingCarName()
+    val userCarLit = convertUserListToCar(UserInput().racingCarName())
     val tryCount = UserInput().tryCount()
+}
+
+private fun convertUserListToCar(userList : List<String>) : List<Car>{
+    val carList = mutableListOf<Car>()
+    userList.forEach { name ->
+        carList.add(Car(name, 0))
+    }
+    return carList
 }
 
